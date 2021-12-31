2bfad6b636fab164a52cf6de28ed385e4e199277
Samarth Shrivastava <26189104+shrivastavasamarth22@users.noreply.github.com>
2021-12-31 16:25:52 +0530

    Started to implement Graham's Scan algorithm for beat polygon

--- Functions/sortPoints.js
+++ Functions/sortPoints.js
@@ -1,3 +1,8 @@
+// I will use the Graham's Scan algorithm to find the convex hull of the beat polygon
+
 export const sortPoints = points => {
+    // Select the rightmost lowest point P0 in S
     const P0 = {x: 0, y: 0};
+
+
 }
